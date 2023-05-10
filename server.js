8abd549c6241860badbbdba294870f2bcda3aae6
Chuqiao Wang <chuqiaowang@Chuqiaos-MBP.fios-router.home>
2023-05-09 21:53:09 -0400

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mime = require('mime');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Replace this with your PostgreSQL connection string
 const connectionString = process.env.postgresql-flat-85578 || 'postgres://user:password@localhost/db_name';
